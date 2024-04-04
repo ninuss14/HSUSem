21484d4634e5ff23ca6da6e97faa359b7ebc81fd
Nina Kostihová <kostihova@isw.sk>
2024-04-04 11:47:46 +0200

    feat: added todo's

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ test_images = loader.load_images_from_folder_test("dataset/Test")
 print(train_images.shape)
 print(test_images.shape)
 
+# TODO nacitat do tensorov
+# TODO Data preprocessing - vsetky na rovnaku velkost/cierno-biele <-> farebne
+# TODO navrhnut zakladnu konvolucnu neuronovu siet - vyskusat rozne architektury / rozne vrstvy
+# TODO trenovanie - early stopping, skusit dynamicky znizovt/zvysovat learning rate, vyskusat rozne optimizers...
+# TODO vyhodnotit spolahlivost - confusion matrix, MSE...
+# TODO skusit pouzit early stopping, znizit pocet vah, skusit farebne obrazky
+
 # plot 10tich trenovacich obrazkov z roznych kategorii
 for i in range(10):
     plt.subplot(2, 5, i + 1)
