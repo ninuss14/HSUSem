6228f9fdcc68f8bad93fdfb304996d8c4f928ca7
Nina Kostihová <kostihova@isw.sk>
2024-04-05 09:33:19 +0200

    feat: load test data with labels

--- loader.py
+++ loader.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import torch
+import pandas as pd
 from torchvision import transforms
 import numpy as np
 
@@ -25,15 +26,29 @@ def load_image_from_folder_train(folder, image_size=(32, 32)):
     return images_tensor, categories_tensor
 
 
-# load metoda pre testovacie data bez kategorii
+def load_csv_data(csv_file):
+    csv_data = pd.read_csv(csv_file)
+    return csv_data
+
+
+# load metoda pre testovacie data
 def load_images_from_folder_test(folder, image_size=(32, 32)):
     images = []
+    categories = []
+    csv_data = load_csv_data('dataset/Test.csv')
+
     for filename in os.listdir(folder):
         image_path = os.path.join(folder, filename)
         img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
         if img is not None:
             img_resized = cv2.resize(img, image_size)  # obrazky nie su rovnakej velkosti
             images.append(img_resized)
 
     images_tensor = torch.stack([transforms.ToTensor()(img) for img in images])
-    return images_tensor
+
+    for index, row in csv_data.iterrows():
+        class_id = row['ClassId']
+        categories.append(class_id)
+
+    return images_tensor, categories
--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import loader
 
 # nacitanie do tensorov
-train_images, categories = loader.load_image_from_folder_train('dataset/Train')
-test_images = loader.load_images_from_folder_test("dataset/Test")
+train_images, train_categories = loader.load_image_from_folder_train('dataset/Train')
+test_images, test_categories = loader.load_images_from_folder_test("dataset/Test")
 
-# TODO Data preprocessing - vsetky na rovnaku velkost/cierno-biele <-> farebne
+
 # TODO navrhnut zakladnu konvolucnu neuronovu siet - vyskusat rozne architektury / rozne vrstvy
 # TODO trenovanie - early stopping, skusit dynamicky znizovt/zvysovat learning rate, vyskusat rozne optimizers...
 # TODO vyhodnotit spolahlivost - confusion matrix, MSE...
@@ -15,7 +15,7 @@ test_images = loader.load_images_from_folder_test("dataset/Test")
 for i in range(10):
     plt.subplot(2, 5, i + 1)
     plt.imshow(train_images[i * 3000].reshape(32, 32), cmap='gray')  # max je 39209
-    plt.title(f"Cat.: {categories[i * 3000]}")
+    plt.title(f"Cat.: {train_categories[i * 3000]}")
     plt.axis('off')
 plt.show()
 
@@ -23,5 +23,6 @@ plt.show()
 for i in range(10):
     plt.subplot(2, 5, i + 1)
     plt.imshow(test_images[i * 500].reshape(32, 32), cmap='gray')  # max je 12630
+    plt.title(f"Cat.: {test_categories[i]}")
     plt.axis('off')
 plt.show()
