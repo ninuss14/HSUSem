75c0a6f25c0d3e57f55e2b24b9bd9b43fe761253
Nina Kostihová <kostihova@isw.sk>
2024-04-05 13:50:23 +0200

    feat: update categories

--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-import matplotlib.pyplot as plt
-from torch import optim
 import numpy as np
 import torch.nn as nn
+from torch import optim
 from torch.utils.data import DataLoader
+
+import custom_dataset
+import evaluator
 import loader
 import neural_network
-import evaluator
-import custom_dataset
-
-# nacitanie do tensorov
-train_images, train_categories = loader.load_image_from_folder_train('dataset/Train')
-test_images, test_categories = loader.load_images_from_folder_test("dataset/Test")
-
-train_dataset = custom_dataset.CustomDataset(train_images, train_categories)
-test_dataset = custom_dataset.CustomDataset(test_images, test_categories)
 
 # TODO navrhnut zakladnu konvolucnu neuronovu siet - vyskusat rozne architektury / rozne vrstvy
 # TODO trenovanie - early stopping, skusit dynamicky znizovt/zvysovat learning rate, vyskusat rozne optimizers...
 # TODO vyhodnotit spolahlivost - confusion matrix, MSE...
 # TODO skusit pouzit early stopping, znizit pocet vah, skusit farebne obrazky
 
-# plot 10tich trenovacich obrazkov z roznych kategorii
-for i in range(10):
-    plt.subplot(2, 5, i + 1)
-    plt.imshow(train_images[i * 3000].reshape(32, 32), cmap='gray')  # max je 39209
-    plt.title(f"Cat.: {train_categories[i * 3000]}")
-    plt.axis('off')
-plt.show()
+# nacitanie do tensorov
+train_images, train_categories = loader.load_image_from_folder_train('dataset/Train')
+test_images, test_categories = loader.load_images_from_folder_test("dataset/Test")
 
-# plot 10tich testovacich obrazkov
-for i in range(10):
-    plt.subplot(2, 5, i + 1)
-    plt.imshow(test_images[i * 500].reshape(32, 32), cmap='gray')  # max je 12630
-    plt.title(f"Cat.: {test_categories[i]}")
-    plt.axis('off')
-plt.show()
+# spojit obrazky s kategoriami
+train_dataset = custom_dataset.CustomDataset(train_images, train_categories)
+test_dataset = custom_dataset.CustomDataset(test_images, test_categories)
 
+# loadovanie batch-u dat
 train_loader = DataLoader(train_dataset, 1000, False)
 test_loader = DataLoader(test_dataset, 100, False)
 
