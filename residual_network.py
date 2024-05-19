f49481f670f6186956db8c377f723a496fddf704
kuruczova <kuruczova@isw.sk>
2024-05-19 21:02:39 +0200

    feat: improved residual network

--- residual_network.py
+++ residual_network.py
@@ -1,8 +1,14 @@
 import torch.nn as nn
 
 
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        m.bias.data.fill_(0.01)
+
+
 def conv3x3(in_planes, out_planes, stride=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1)
 
 
 class BasicBlock(nn.Module):
@@ -13,10 +19,12 @@ class BasicBlock(nn.Module):
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(inplace=True)
+        # self.dropout = nn.Dropout2d()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
+        self.apply(init_weights)
 
     def forward(self, x):
         residual = x
@@ -24,6 +32,7 @@ class BasicBlock(nn.Module):
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
+        # out = self.dropout(out)
         out = self.conv2(out)
         out = self.bn2(out)
 
@@ -38,31 +47,35 @@ class BasicBlock(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes=43):
-        self.inplanes = 20
+        self.inplanes = 7
         super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(20)
+        self.conv1 = nn.Conv2d(1, 7, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(7)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)
-        self.layer1 = self._make_layer(block, 20, layers[0], stride=1)
-        self.layer2 = self._make_layer(block, 40, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 60, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 80, layers[3], stride=2)
+        self.layer1 = self._make_layer(block, 7, layers[0], stride=1)
+        self.layer2 = self._make_layer(block, 9, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 15, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 20, layers[3], stride=2)
+        self.layer5 = self._make_layer(block, 23, layers[4], stride=2)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(80 * block.expansion, num_classes)
+        self.dropout = nn.Dropout()
+        self.fc = nn.Linear(23 * block.expansion, num_classes)
 
+        # inicializacia vah, bias pre vsetky vrstvy
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                m.bias.data.fill_(0.01)
 
     def _make_layer(self, block, planes, blocks, stride=1):
         downsample = None
+        # menime velkost len nam nesedia input a output
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion, kernel_size=1, stride=stride),
                 nn.BatchNorm2d(planes * block.expansion),
             )
 
@@ -83,8 +96,10 @@ class ResNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
+        x = self.layer5(x)
 
         x = self.avgpool(x)
+        x = self.dropout(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
